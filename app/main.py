94a4d3773dd84e96024d677657d0e140782fac1b
Ceres <42400528+Ceres445@users.noreply.github.com>
2022-05-17 12:16:10 +0530

    Changed root to render a webpage

--- app/main.py
+++ app/main.py
@@ -1,19 +1,35 @@
-from fastapi import FastAPI, HTTPException
-from helpers.spotify import SpotifyClient
-from helpers.convertor import ConvertorClient
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+from pathlib import Path
 
 # from ytmusic.recommendations import get_recommendations
 from ytmusic.search import get_search_results
 from app.models import Song
+from helpers.spotify import SpotifyClient
+from helpers.convertor import ConvertorClient
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+BASE_DIR = Path(__file__).resolve().parent
+
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, "static")))
 spotify_client = SpotifyClient()
 converter = ConvertorClient()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("root.html", {"request": request})
+
+
+@app.get("/loaderio-731f70c007102cf34840aaa849f32da3.txt", include_in_schema=False)
+async def loader():
+    return FileResponse("app/static/loaderio-731f70c007102cf34840aaa849f32da3.txt")
 
 
 @app.get(
@@ -31,7 +47,7 @@ async def search(query: str, limit: int = 10) -> list[Song]:
     response_model=list[Song],
     response_description="Returns youtube raw data results of multiple songs using the old api",
 )
-async def test_api(songs: list[str]) -> list[Song]:
+async def get_songs_api(songs: list[str]) -> list[Song]:
     songs = await converter.get_songs(songs)
     if songs is None:
         raise HTTPException(status_code=404, detail="playlist not found on youtube")
@@ -43,7 +59,7 @@ async def test_api(songs: list[str]) -> list[Song]:
     response_model=list[Song],
     response_description="Returns youtube raw data results of multiple songs",
 )
-async def test(songs: list[str]):
+async def get_songs(songs: list[str]):
     if not songs:
         raise HTTPException(status_code=400, detail="Song is required")
     return [get_search_results(song, lim=1)[0] for song in songs]
@@ -57,11 +73,12 @@ async def test(songs: list[str]):
 async def get_song(songs: str):
     if not songs:
         raise HTTPException(status_code=400, detail="Song is required")
+    # TODO: use multithreading here to fetch all results at once
     return [get_search_results(song, lim=1)[0] for song in songs]
 
 
 @app.get(
-    "/spotify_playlist/{playlist_id}",
+    "/spotify_playlist/",
     response_model=list[Song],
     response_description="Returns youtube raw data results of songs in a spotify playlist",
 )
